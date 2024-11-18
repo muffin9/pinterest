5937ea2bbc978b4053bf8b58d8549622cc6e5077
muffin9 <jinha6707@gmail.com>
2024-11-18 23:20:01 +0900

    🍀 refactor: remove not used component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,4 +2,3 @@ export { CommonHeader } from "./common/header";
 export { Nav } from "./common/nav";
 export { Banner } from "./Banner";
 export { PaginationFooter } from "./common/pagination";
-export { Modal } from "./common/Modal";
